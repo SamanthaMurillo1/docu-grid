aa373ff30ba3239a64ace2597906f22fe135861c
sam50-commits <sam50@my.yorku.ca>
2026-07-20 22:14:30 -0400

    sign in connected with firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,6 +4,14 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
  // private firebase setup add code below this!
+    apiKey: "AIzaSyCo6kEHOrdWNNZBiJIT1TI0lJsDmMqaQao",
+    authDomain: "docu-signup.firebaseapp.com",
+    projectId: "docu-signup",
+    storageBucket: "docu-signup.firebasestorage.app",
+    messagingSenderId: "909147596709",
+    appId: "1:909147596709:web:fef2889d707a8105dfea4c",
+
+
 };
 
 export const app = initializeApp(firebaseConfig);
